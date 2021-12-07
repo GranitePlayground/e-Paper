bc453248eef616cc65c39b49481735de0e6920e7
colin <GranitePlayground@gmail.com>
2021-12-06 19:39:43 -0500

    project update

--- RaspberryPi_JetsonNano/python/examples/e-Inkrapidtest.py
+++ RaspberryPi_JetsonNano/python/examples/e-Inkrapidtest.py
@@ -23,42 +23,15 @@ try:
     epd.init(epd.FULL_UPDATE)
     epd.Clear(0xFF)
 
-    # Drawing on the image
-    font15 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 15)
-    font24 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 24)
-    
-    # read bmp file 
-    logging.info("2.read bmp file...")
-    image = Image.open(os.path.join(picdir, '2in13.bmp'))
-    epd.display(epd.getbuffer(image))
-    time.sleep(2)
-    
     # read bmp file on window
     logging.info("3.read bmp file on window...")
     # epd.Clear(0xFF)
     image1 = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame
-    bmp = Image.open(os.path.join(picdir, '100x100.bmp'))
+    bmp = Image.open(os.path.join(picdir, '2.66inch-4.bmp'))
     image1.paste(bmp, (2,2))    
     epd.display(epd.getbuffer(image1))
     time.sleep(2)
-    
-    # # partial update
-    logging.info("4.show time...")
-    time_image = Image.new('1', (epd.height, epd.width), 255)
-    time_draw = ImageDraw.Draw(time_image)
-    
-    epd.init(epd.FULL_UPDATE)
-    epd.displayPartBaseImage(epd.getbuffer(time_image))
-    
-    epd.init(epd.PART_UPDATE)
-    num = 0
-    while (True):
-        time_draw.rectangle((120, 80, 220, 105), fill = 255)
-        time_draw.text((120, 80), time.strftime('%H:%M:%S'), font = font24, fill = 0)
-        epd.displayPartial(epd.getbuffer(time_image))
-        num = num + 1
-        if(num == 10):
-            break
+
     # epd.Clear(0xFF)
     logging.info("Clear...")
     epd.init(epd.FULL_UPDATE)
